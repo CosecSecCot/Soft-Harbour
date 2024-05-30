2b793980ae612e180aae689db48ab0fe2bfc57ae
CosecSecCot <cosecseccot581@gmail.com>
2024-05-30 11:00:27 +0530

    feat: prisma setup

---
+++ src/app/admin/layout.tsx
@@ -0,0 +1,19 @@
+import { AdminNav, AdminNavLink } from "@/components/admin-nav";
+
+export default function AdminLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <>
+            <AdminNav>
+                <AdminNavLink href="/admin">Dashboard</AdminNavLink>
+                <AdminNavLink href="/admin/products">Products</AdminNavLink>
+                <AdminNavLink href="/admin/users">Customers</AdminNavLink>
+                <AdminNavLink href="/admin/orders">Sales</AdminNavLink>
+            </AdminNav>
+            <div className="container my-6">{children}</div>
+        </>
+    );
+}
---
+++ src/components/admin-nav.tsx
@@ -0,0 +1,35 @@
+"use client";
+import React, { ComponentProps } from "react";
+import { ModeToggle } from "./mode-toggle";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+import { cn } from "@/lib/utils";
+
+export function AdminNav({ children }: { children: React.ReactNode }) {
+    return (
+        <nav className="bg-primary text-primary-foreground flex justify-between items-center pr-4">
+            <div className="bg-primary text-primary-foreground flex justify-start">
+                {children}
+            </div>
+            <span>
+                <ModeToggle />
+                <span className="sr-only">Theme Button</span>
+            </span>
+        </nav>
+    );
+}
+
+export function AdminNavLink(
+    props: Omit<ComponentProps<typeof Link>, "className">
+) {
+    const pathName = usePathname();
+    return (
+        <Link
+            {...props}
+            className={cn(
+                "p-4 hover:bg-secondary hover:text-secondary-foreground focus-visible:bg-secondary focus-visible:text-secondary-foreground",
+                pathName === props.href ? "bg-background text-foreground" : ""
+            )}
+        />
+    );
+}
